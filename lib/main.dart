f07aec692543651184925eee9e5566f479c21259
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-07-09 17:39:13 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_movies/screens/screens.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Peliculas',
+      initialRoute: 'homeScreen',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        'homeScreen': (context) => const HomeScreen(),
+        'detailScreen': (context) => const DetailsScreen()
+      },
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_application_movies/screens/details_screen.dart';
+export 'package:flutter_application_movies/screens/home_screen.dart';
